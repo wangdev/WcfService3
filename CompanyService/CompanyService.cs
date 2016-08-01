38e737e8863d24d804f42b9a71f28a030aaa5bfe
Michael Wang <wangdevelop@gmail.com>
2016-08-01 14:41:18 -0400

    service and service host

---
+++ CompanyService/CompanyService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace CompanyService {
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    public class CompanyService : ICompanyPublicService, ICompanyConfidentialService {
+        public string GetConfidentialInfo() {
+            return "This is public information and available over HTTP to the public";
+        }
+
+        public string GetPublicInfo() {
+            return "This is confidential info and available over TCP to the company employees";
+        }
+    }
+}
---
+++ CompanyServiceHost/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using CompanyService;
+
+namespace CompanyServiceHost {
+    class Program {
+        static void Main(string[] args) {
+            using (ServiceHost host = new ServiceHost(typeof(CompanyService.CompanyService))) {
+                host.Open();
+                Console.WriteLine("Host started @ " + DateTime.Now);
+                Console.ReadLine();
+            }
+        }
+    }
+}
